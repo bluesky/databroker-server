36518a825b05c443b7c3abba9dcee221ab68ed49
Marcus D. Hanwell <mhanwell@bnl.gov>
2020-07-31 13:29:38 -0400

    Fill in the method to get streams for a run
    Borrows a little from bluesky-widgets to determine if we have a run or a
    catalog of catalogs.

--- databroker_server/model/databroker.py
+++ databroker_server/model/databroker.py
@@ -1,5 +1,12 @@
 from databroker import catalog
 
+def _has_runs(catalog):
+    """Borrowed from bluesky-widgets, see if we are at a 'leaf'."""
+    from databroker.v2 import Broker
+
+    return isinstance(catalog, Broker)
+
+
 def catalogs():
     """Get a list of catalogs."""
     return list(catalog)
@@ -14,4 +21,11 @@ def runs(name: str):
 
 def streams(catalog_id: str, uid: str):
     """Get a list of streams from the run."""
-    
+    try:
+        current = catalog[catalog_id]
+    except KeyError:
+        return None
+    if _has_runs(current):
+        return list(current[uid])
+
+    return None
